9f15793ce910cd78c31c1529547b4c40d7ff1281
Courtney Daly <courtneydaly1@gmail.com>
2024-08-11 13:28:39 -0400

    update axios into pokemon cards

--- src/PlayingCardList.js
+++ src/PlayingCardList.js
@@ -15,8 +15,8 @@ function CardTable() {
     <div className="PlayingCardList">
       <h3>Pick a card, any card!</h3>
       <div>
-        <button onClick={addCard}>Add a playing card!</button>
-        <button onClick={clearCards}>Clear the table</button>
+      <button onClick={() => addCard(formatCard)}>Add a playing card!</button>
+      <button onClick={clearCards}>Clear the table</button>
       </div>
       <div className="PlayingCardList-card-area">
         {cards.map(cardData => (
