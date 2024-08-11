a79415ccc84adfddcef9cef06846de97d2846c77
Courtney Daly <courtneydaly1@gmail.com>
2024-08-11 12:53:28 -0400

    more hooks

--- src/hooks.js
+++ src/hooks.js
@@ -10,3 +10,32 @@ function useFlip(initialFlipState = true){
     return [isFlipped, flip];
 }
 
+function useAxios(key, baseURL){
+    const [response, setResponse] = useLocalStorage(key);
+
+    const addResData = async (formatter = data => data, restOfURL = "") =>{
+        const response = await axios.get(`${baseURL}${restOfURL}`);
+        setResponse(data => [...data, formatter(response.data)]);
+    };
+    const clearResp = () => {
+        setResponse([]);    
+    };
+
+    return [response, addResData, clearResp]
+}
+
+function useLocalStorage(key, initialValue= []){
+    if(localStorage.getItem(key)){
+        initialValue = JSON.parse(localStorage.getItem(key));
+    };
+    const [value, setValue] = useState(initialValue);
+
+    useEffect(() => {
+        localStorage.setItem(key, JSON.stringify(value));
+    }, [value, setValue]);
+}
+
+export default useLocalStorage;
+
+export { useAxios, useFlip, useLocalStorage };
+
